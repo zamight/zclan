0337df39baa46d145bc5488b2d8cdb14597ef291
Cody Wofford <cody.e.wofford@gmail.com>
2016-06-08 20:22:35 -0400

    Forum Basic Setup

--- class/forum.class.php
+++ class/forum.class.php
@@ -1,14 +1,46 @@
 <?php
 
-class user
+class forum
 {
 
-    //Variables.
-    private $arr = array();
-
-    function __construct()
+    public function index($clanName = "Site")
     {
+        global $db;
 
+        $templateList = "forum_index";
+        $templateListArray = explode(',', $templateList);
+
+        $html = "";
+
+        //Setup Template Variables.
+        foreach ($templateListArray as $templateName) {
+            $$templateName = $db->getTemplate($templateName);
+        }
+
+         // If the $clanName is set
+         // Then set the $clanID to to the clanNames ID.
+
+        $clan = $db->fetchQuery("SELECT * FROM `clan` WHERE `name` = '{$clanName}'");
+        $clan = $clan[0];
+
+         // If the clan cannot be found
+         // Then run the invalid_clan function.
+
+        if(!$clan) {
+            $this->invalid_clan();
+        }
+
+         // Set clan information for template.
+
+         // Display the default layout.
+
+        eval("\$html .= \"$forum_index\";");
+
+        echo $html;
+    }
+
+    private function invalid_clan()
+    {
 
     }
 
