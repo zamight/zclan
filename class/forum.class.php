e315d94d4d3ed0f6d69884d7d5bf2846df6c10bf
Cody Wofford <cody.e.wofford@gmail.com>
2016-06-11 18:34:08 -0400

    work

--- class/forum.class.php
+++ class/forum.class.php
@@ -1,11 +1,12 @@
 <?php
 
+
 class forum
 {
 
     public function index($clanName = "Site")
     {
-        global $db;
+        global $db, $classMethod;
 
         $templateList = "forum_index";
         $templateListArray = explode(',', $templateList);
@@ -34,9 +35,43 @@ class forum
 
          // Display the default layout.
 
+        //Lets Setup and Display category Empty
+        $body = $this->index_categorys($clan['id']);
+
         eval("\$html .= \"$forum_index\";");
 
         echo $html;
+    }
+
+    private function index_categorys($clanId = 1)
+    {
+	global $db;
+
+	//Load Layout.
+        $tpl = $db->getTemplate("forum_index_category");
+        $tplForum = $db->getTemplate("forum_index_forum");
+        $html = '';
+
+        //Template is Loaded.
+        // Lets Loop All the Categorys.
+        $category_sql = 'SELECT * FROM category WHERE clanID = ' . $clanId;
+        $categorys = $db->fetchQuery($category_sql);
+
+        foreach($categorys as $category) {
+            //Lets Load Each Forum Now.
+            $forum_sql = 'SELECT * FROM forum WHERE categoryID = ' . $category['id'];
+            $forums = $db->fetchQuery($forum_sql);
+
+            $forum_html = '';
+
+            foreach($forums as $forum) {
+                eval("\$forum_html .= \"$tplForum\";");
+            }
+
+            eval("\$html .= \"$tpl\";"); 
+        } 
+
+        return $html;
     }
 
     private function invalid_clan()
